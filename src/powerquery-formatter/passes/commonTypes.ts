3a8c1abe9a1483dddf399c6eeac648dbaac11da5
JordanBoltonMN <jordanboltonmn@gmail.com>
2021-06-24 10:55:37 -0700

    initial commit

--- src/powerquery-formatter/passes/commonTypes.ts
+++ src/powerquery-formatter/passes/commonTypes.ts
@@ -15,23 +15,23 @@ export interface CommentCollection {
     prefixedCommentsContainsNewline: boolean;
 }
 
-export interface CommentState extends PQP.Traverse.IState<CommentCollectionMap> {
+export interface CommentState extends PQP.Traverse.ITraversalState<CommentCollectionMap> {
     readonly comments: ReadonlyArray<PQP.Language.Comment.TComment>;
     commentsIndex: number;
     maybeCurrentComment: PQP.Language.Comment.TComment | undefined;
 }
 
-export interface IsMultilineFirstPassState extends PQP.Traverse.IState<IsMultilineMap> {
+export interface IsMultilineFirstPassState extends PQP.Traverse.ITraversalState<IsMultilineMap> {
     readonly commentCollectionMap: CommentCollectionMap;
     readonly nodeIdMapCollection: PQP.Parser.NodeIdMap.Collection;
     readonly linearLengthMap: LinearLengthMap;
 }
 
-export interface IsMultilineSecondPassState extends PQP.Traverse.IState<IsMultilineMap> {
+export interface IsMultilineSecondPassState extends PQP.Traverse.ITraversalState<IsMultilineMap> {
     readonly nodeIdMapCollection: PQP.Parser.NodeIdMap.Collection;
 }
 
-export interface LinearLengthState extends PQP.Traverse.IState<number> {
+export interface LinearLengthState extends PQP.Traverse.ITraversalState<number> {
     readonly nodeIdMapCollection: PQP.Parser.NodeIdMap.Collection;
     readonly linearLengthMap: LinearLengthMap;
 }
@@ -60,7 +60,7 @@ export interface SerializeParameterMap {
     readonly comments: Map<number, ReadonlyArray<SerializeCommentParameter>>;
 }
 
-export interface SerializeParameterState extends PQP.Traverse.IState<SerializeParameterMap> {
+export interface SerializeParameterState extends PQP.Traverse.ITraversalState<SerializeParameterMap> {
     readonly nodeIdMapCollection: PQP.Parser.NodeIdMap.Collection;
     readonly commentCollectionMap: CommentCollectionMap;
     readonly isMultilineMap: IsMultilineMap;
